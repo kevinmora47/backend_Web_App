49b1263a112e167d50876ec318d584b65c9b876a
kevinmora47 <kevinmora47@gmail.com>
2020-09-16 22:26:59 -0600

    Task #2-Connection to the database-16-09-2020

--- Electronic_Business/Electronic_Business/Program.cs
+++ Electronic_Business/Electronic_Business/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Data;
+using System.Net.WebSockets;
 
 namespace Electronic_Business
 {
@@ -13,6 +17,29 @@ namespace Electronic_Business
     {
         public static void Main(string[] args)
         {
+            var ds = new DataSet();
+            var da = new SqlDataAdapter();
+            var cmd = new SqlCommand();
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "dbis.database.windows.net";
+            builder.UserID = "kmorav";
+            builder.Password = "Jksbwn66";
+            builder.InitialCatalog = "DBIS";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("=========================================\n");
+                    connection.Open();
+                    Console.WriteLine(connection.State);
+                    cmd.CommandText = "Search_Customers";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand = cmd;
+                    da.Fill(ds);
+                    
+                
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
